c9fdb55c2889b5832ccbea77be28c8553be292d0
André Gonçalves <47006482+andgonn@users.noreply.github.com>
2023-12-22 11:12:23 -0300

    Add files via upload

---
+++ src/main/java/exemplo/host/integrator/integratorexemplodeploygit/IntegratorExemploDeployGitApplication.java
@@ -0,0 +1,13 @@
+package exemplo.host.integrator.integratorexemplodeploygit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegratorExemploDeployGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegratorExemploDeployGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/exemplo/host/integrator/integratorexemplodeploygit/IntegratorExemploDeployGitApplicationTests.java
@@ -0,0 +1,13 @@
+package exemplo.host.integrator.integratorexemplodeploygit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratorExemploDeployGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
